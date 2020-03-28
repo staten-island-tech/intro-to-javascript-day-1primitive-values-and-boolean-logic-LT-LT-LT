70f29826d85f19f89d95c0499d7fc1fb718340a8
LT-LT-LT <57498881+LT-LT-LT@users.noreply.github.com>
2020-03-28 01:43:29 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+let light = "green";
+if(light=="green"){
+console.log("Go");
+} else if {light=="yellow"){
+console.log("Caution");
+} else (
+console.log("Stop")};
+
+let Kristy = "Lactose";
+let Bao_Bao = "Lactose";
+
+if(Kristy=="Lactose" && Bao_Bap=="Lactose"){
+console.log("Milk");
+} else if {Kristy=="Lactose" || Bao_Bap=="Lactose")){
+console.log("Both Milk");
+} else (
+console.log("Almond Milk");};
